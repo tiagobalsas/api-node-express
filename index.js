bd0ed59ad16ac7b9badeafd9d26d4c73c2b29e5e
Tiago Santos <tiago881@gmail.com>
2021-08-31 15:20:27 -0300

    Cria rota e cria o método GET http

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
+// const bodyParser = require('body-parser');
+
+const userRaute = require('./routes/userRoute');
+
 const app = express();
 const porta = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+userRaute(app);
 
 app.get('/', (req, res) => {
   res.send('<h1>Respondendo a requisição</h1>');
---
+++ routes/userRoute.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const { join } = require('path');
+
+const filePath = join(__dirname, 'users.json');
+
+const getUsers = () => {
+  const data = fs.existsSync(filePath) ? fs.readFileSync(filePath) : [];
+  try {
+    return JSON.parse(data);
+  } catch (error) {
+    return [];
+  }
+};
+
+const saveUser = (users) => {
+  fs.writeFileSync(filePath, JSON.stringify(users, null, '\t'));
+};
+
+const userRaute = (app) => {
+  app
+    .route('/users/:id?')
+    .get((req, res) => {
+      const users = getUsers();
+      res.send({ users });
+    })
+};
+
+module.exports = userRaute;
